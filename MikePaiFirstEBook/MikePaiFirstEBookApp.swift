80b49067e44ce285f623567d8a6ce29efae2457d
MikePai <ye518612@gmail.com>
2020-10-16 20:12:57 +0800

    Initial Commit

---
+++ MikePaiFirstEBook/MikePaiFirstEBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  MikePaiFirstEBookApp.swift
+//  MikePaiFirstEBook
+//
+//  Created by 白謹瑜 on 2020/10/16.
+//
+
+import SwiftUI
+
+@main
+struct MikePaiFirstEBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
